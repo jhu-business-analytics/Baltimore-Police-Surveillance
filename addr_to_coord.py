18c7236c9695cd65236defd54ae0165a612f675a
Reece W Griffith <rgriff43@jhu.edu>
2019-10-22 20:30:37 -0400

    cleaned crime data

--- addr_to_coord.py
+++ addr_to_coord.py
@@ -2,28 +2,27 @@ from geopy.geocoders import Nominatim
 import csv
 import re
 
-f = open("coords.csv", "a")
+f = open("crime_coords.csv", "a")
 regex = re.compile('[^\w ]')
 
-with open('addresses.csv') as csvfile:
+with open('crime_addr.csv') as csvfile:
 	readCSV = csv.reader(csvfile, delimiter=',')
 	count = 0
 	for row in readCSV:
 		
 		if count == 0:
-			row = "900 S CATON AV"
+			row = "1200 UNION AV"
 		else:
 			row = str(row)
 			row = regex.sub('', row)
 		count += 1
 
 		address = row +  " Baltimore, MD"
-		geolocator = Nominatim(user_agent="specify_your_app_name_here",timeout=1000)
+		geolocator = Nominatim(user_agent="specify_your_app_name_here",timeout=1)
 		location = geolocator.geocode(address)
 
 		if location is not None:
-			#print(row)
-			#print((location.latitude, location.longitude))
-			
-			f.write(str((location.latitude, location.longitude)) + "\n")
+			f.write(str((row, location.latitude, location.longitude)) + "\n")
+		else:
+			f.write("\n")
 f.close()
